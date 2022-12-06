18ed123fd00b0819d729f4d6d418091d512cca1e
Olga Rudson <o.rudson@mail.ru>
2022-12-06 11:01:20 +0300

    решение2

---
+++ example1.1/Program.cs
@@ -0,0 +1,6 @@
+﻿  string[] arraytocheck = { "a", "32", "435", "912123", "ok" };
+  Console.WriteLine("Дано: " + String.Join(",", arraytocheck));
+  
+  arraytocheck = arraytocheck.Where(element => element.Length <= 3).ToArray();
+  Console.WriteLine("Выдано: " + String.Join(",", arraytocheck));
+ 
